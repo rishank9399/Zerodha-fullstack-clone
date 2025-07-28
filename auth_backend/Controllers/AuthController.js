2484a4a8eea0d015d1f75523a7c42e39b2ce57ec
rishank9399 <116252920+rishank9399@users.noreply.github.com>
2025-07-29 02:54:22 +0530

    Resolving Bugs

--- auth_backend/Controllers/AuthController.js
+++ auth_backend/Controllers/AuthController.js
@@ -40,6 +40,7 @@ module.exports.Login = async (req, res, next) => {
       return res.json({message:'Incorrect password or email' }) 
     }
      const token = createSecretToken(user._id);
+     console.log(token);
      res.cookie("token", token, {
        httpOnly: true,
        secure: true,
